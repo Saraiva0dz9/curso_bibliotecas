2cc942402dc283eabf2a2e1dfd8b48973933df0a
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2023-04-18 19:58:05 -0300

    Feat: Definindo método interno e declarando ele em outra classe

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,8 +1,24 @@
-﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+﻿using bytebank.Modelos.ADM.Funcionarios;
+using bytebank_ATENDIMENTO.bytebank.Atendimento;
 using bytebank_Models.bytebank.Modelos.ADM.Utilitario;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 new ByteBankAtendimento().AtendimentoCliente();
 
+public class Estagiario : Funcionario
+{
+    public Estagiario(double salario, string cpf) : base(salario, cpf)
+    {
 
-AutenticacaoUtil autenticador = new AutenticacaoUtil();
+    }
+
+    public override void AumentarSalario()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override double getBonificacao()
+    {
+        throw new NotImplementedException();
+    }
+}
